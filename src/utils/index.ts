3b546c4257162261453ebc1d3053c89053a0bf43
Radu Lupaescu <radu.lupaescu@gmail.com>
2023-10-11 10:23:51 +0300

    fix: expose helper functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,5 @@ export * from './aixp.deserializer';
 export * from './aixp.message.transformer';
 export * from './aixp.pseudopy.helpers';
 export * from './aixp.serializer';
+export * from './aixp.bc';
+export * from './aixp.helper.functions';
